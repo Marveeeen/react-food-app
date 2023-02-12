44a9115a1ec88f213bbe240dea490dfbf4b39c00
Marveeen <jmvroqueee@gmail.com>
2023-02-12 10:52:43 +0800

    added dummy data for the menu and changed price to pesos

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -4,7 +4,7 @@ import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
+  const price = `₱${props.price.toFixed(2)}`;
 
   return (
     <li className={classes.meal}>
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+export const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sinigang",
+    description:
+      "A tamarind-based soup that is known for its sour and savory taste",
+    price: 210,
+  },
+  {
+    id: "m2",
+    name: "Adobo",
+    description:
+      "Slow-cooked pork in a tangy sauce of soy, vinegar, garlic, bay leaves and peppercorns",
+    price: 190,
+  },
+  {
+    id: "m3",
+    name: "Lechon",
+    description:
+      "A whole roasted pig, often served at special occasions and celebrations.",
+    price: 250,
+  },
+  {
+    id: "m4",
+    name: "Kare-Kare",
+    description:
+      "A hearty stew made with oxtail and vegetables in a rich peanut sauce.",
+    price: 230,
+  },
+];
